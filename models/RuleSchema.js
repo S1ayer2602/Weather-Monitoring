54c66c42d55013361f68794db8b1e30b56b9dc00
S1ayer2602 <100183354+S1ayer2602@users.noreply.github.com>
2024-07-28 21:50:06 +0530

    Add files via upload

--- models/RuleSchema.js
+++ models/RuleSchema.js
@@ -1,1 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
+const nodeSchema = new Schema({
+  type: String,
+  left: { type: Schema.Types.Mixed, default: null },
+  right: { type: Schema.Types.Mixed, default: null },
+  value: { type: String, default: null },
+});
+
+const ruleSchema = new Schema({
+  ruleString: String,
+  ast: nodeSchema,
+});
+
+module.exports = mongoose.model("Rule", ruleSchema);
